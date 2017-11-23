fba2129250f8fb26982f7e05cb721d92db4d4602
Mateusz Lawczys <ma6tek@gmail.com>
2017-11-23 23:34:25 +0100

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+var a = prompt('Podaj podstawę trójkąta:'),
+    h = prompt('Podaj wysokość trójkąta:'),
+    triangleArea = a*h/2;
+alert('Pole trójkąta wynosi: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
